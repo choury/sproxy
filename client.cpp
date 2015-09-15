429d8bd1e76a9b26c5199cef09199936ffc7cb35
choury <choury@debian.choury.com>
2015-09-15 23:44:04 +0800

    新增Server类，使新增服务更容易

--- client.cpp
+++ client.cpp
@@ -9,14 +9,40 @@
 
 int efd;
 
+
+class Http_server: public Server{
+    virtual void defaultHE(uint32_t events){
+        if (events & EPOLLIN) {
+            int clsk;
+            struct sockaddr_in6 myaddr;
+            socklen_t temp = sizeof(myaddr);
+            if ((clsk = accept(fd, (struct sockaddr*)&myaddr, &temp)) < 0) {
+                LOGE("accept error:%s\n", strerror(errno));
+                return;
+            }
+
+            int flags = fcntl(clsk, F_GETFL, 0);
+            if (flags < 0) {
+                LOGE("fcntl error:%s\n", strerror(errno));
+                close(clsk);
+                return;
+            }
+
+            fcntl(clsk, F_SETFL, flags | O_NONBLOCK);
+            new Guest(clsk, &myaddr);
+        } else {
+            LOGE("unknown error\n");
+        }
+    }
+public:
+    Http_server(int fd):Server(fd){};
+};
+
 #ifdef _ANDROID_
 int Java_com_choury_sproxy_Client_main(JNIEnv *env, jobject, jstring s) {
     const char* srvaddr = (env)->GetStringUTFChars(s, 0);
 
 #else
-
-
-
 int main(int argc, char** argv) {
     if (argc != 2) {
         LOGOUT("Usage: %s Server[:port]\n", basename(argv[0]));
@@ -35,11 +61,7 @@ int main(int argc, char** argv) {
     if ((svsk = Listen(SOCK_STREAM, CPORT)) < 0) {
         return -1;
     }
-
-    struct epoll_event event;
-    event.data.ptr = NULL;
-    event.events = EPOLLIN;
-    epoll_ctl(efd, EPOLL_CTL_ADD, svsk, &event);
+    new Http_server(svsk);
 
     LOGOUT("Accepting connections ...\n");
 #ifndef DEBUG
@@ -55,40 +77,12 @@ int main(int argc, char** argv) {
                 LOGE("epoll wait:%s\n", strerror(errno));
                 return 4;
             }
-
             continue;
         }
 
         for (int i = 0; i < c; ++i) {
-            if (events[i].data.ptr == NULL) {
-                if (events[i].events & EPOLLIN) {
-                    int clsk;
-                    struct sockaddr_in6 myaddr;
-                    socklen_t temp = sizeof(myaddr);
-                    if ((clsk = accept(svsk, (struct sockaddr*)&myaddr, &temp)) < 0) {
-                        LOGE("accept error:%s\n", strerror(errno));
-                        continue;
-                    }
-
-                    int flags = fcntl(clsk, F_GETFL, 0);
-
-                    if (flags < 0) {
-                        LOGE("fcntl error:%s\n", strerror(errno));
-                        close(clsk);
-                        continue;
-                    }
-
-                    fcntl(clsk, F_SETFL, flags | O_NONBLOCK);
-                    new Guest(clsk, &myaddr);
-
-                } else {
-                    LOGE("unknown error\n");
-                    return 5;
-                }
-            } else {
-                Con* con = (Con*)events[i].data.ptr;
-                (con->*con->handleEvent)(events[i].events);
-            }
+            Con* con = (Con*)events[i].data.ptr;
+            (con->*con->handleEvent)(events[i].events);
         }
         
         if(c < 5) {
--- con.h
+++ con.h
@@ -2,6 +2,7 @@
 #define CON_H__
 
 #include <stdint.h>
+#include <sys/epoll.h>
 
 extern int efd;
 
@@ -11,5 +12,18 @@ public:
     virtual ~Con() {}
 };
 
+class Server:public Con{
+protected:
+    int fd;
+    virtual void defaultHE(uint32_t events)=0;
+public:
+    Server(int fd):fd(fd){
+        struct epoll_event event;
+        event.data.ptr = this;
+        event.events = EPOLLIN;
+        epoll_ctl(efd, EPOLL_CTL_ADD, fd, &event);
+        handleEvent = (void (Con::*)(uint32_t))&Server::defaultHE;
+    }
+};
 
 #endif
