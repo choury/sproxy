5cb3df5176c147cc50221b61271e1c252405718e
choury <zhouwei400@gmail.com>
2019-10-13 00:24:32 +0800

    move config file's path

--- systemd/sproxy@.service.cmake
+++ systemd/sproxy@.service.cmake
@@ -5,7 +5,7 @@ After=network.target
 [Service]
 Type=forking
 WorkingDirectory=-/var/lib/sproxy
-ExecStart=@CMAKE_INSTALL_PREFIX@/bin/sproxy -c @CMAKE_INSTALL_PREFIX@/etc/sproxy/%I.conf -D
+ExecStart=@CMAKE_INSTALL_PREFIX@/bin/sproxy -c /etc/sproxy/%I.conf -D
 ExecReload=/bin/kill -HUP $MAINPID
 KillMode=process
 Restart=on-failure
