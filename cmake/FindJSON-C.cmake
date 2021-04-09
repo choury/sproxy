0caf82b8734bca2e8c9ea91630c552656b02ddc5
choury <zhouwei400@gmail.com>
2021-04-10 01:58:43 +0800

    add cmake for readline

--- cmake/FindJSON-C.cmake
+++ cmake/FindJSON-C.cmake
@@ -1,30 +1,28 @@
 # Try to find json-c
 # Once done, this will define
 #
-# JSON-C_FOUND         - system has jsonc
-# JSON-C_INCLUDE_DIRS  - the jsonc include directories
-# JSON-C_LIBRARIES     - jsonc libraries directories
+# JSON-C_FOUND        - system has jsonc
+# JSON-C_INCLUDE_DIR  - the jsonc include directories
+# JSON-C_LIBRARY     - jsonc libraries directories
 
-if(JSON-C_INCLUDE_DIRS AND JSON-C_LIBRARIES)
+if(JSON-C_INCLUDE_DIR AND JSON-C_LIBRARY)
     set(JSONC_FIND_QUIETLY TRUE)
-endif(JSON-C_INCLUDE_DIRS AND JSON-C_LIBRARIES)
+endif()
 
-find_path(JSON-C_INCLUDE_DIR json.h
+find_path(JSON-C_INCLUDE_DIR NAMES json.h
     HINTS
     /usr/include/json-c/
     /usr/local/include/json-c/
     )
 
-find_library(JSON-C_LIBRARY json-c
+find_library(JSON-C_LIBRARY NAMES json-c
     HINTS
     /usr/lib/
     /usr/local/lib
     )
 
-set(JSON-C_INCLUDE_DIRS ${JSON-C_INCLUDE_DIR})
-set(JSON-C_LIBRARIES ${JSON-C_LIBRARY})
 # handle the QUIETLY and REQUIRED arguments and set JSONC_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JSON-C DEFAULT_MSG JSON-C_INCLUDE_DIRS JSON-C_LIBRARIES)
-mark_as_advanced(JSON-C_INCLUDE_DIRS JSON-C_LIBRARIES)
+find_package_handle_standard_args(JSON-C DEFAULT_MSG JSON-C_INCLUDE_DIR JSON-C_LIBRARY)
+mark_as_advanced(JSON-C_INCLUDE_DIR JSON-C_LIBRARY)
